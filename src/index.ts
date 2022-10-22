98d99f4cac79038a67d58112fd04dd8db05a8f7f
takuabonn <k.takumi19980322@gmail.com>
2022-10-22 19:42:12 +0900

    右方向での判定

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,105 @@
-interface PinPosition {
-  [key: number]: number[];
+interface PinPositionalRelationship {
+  [pin: number]: {
+    right: number[][];
+    left: number[][];
+  };
 }
+const pinPositionalRelationship: PinPositionalRelationship = {
+  2: {
+    right: [
+      [1, 5],
+      [3, 9, 6, 10],
+    ],
+    left: [[4], [7]],
+  },
+  3: {
+    right: [[6], [10]],
+    left: [[5], [2, 8, 4, 7]],
+  },
+  4: {
+    right: [
+      [2, 8],
+      [5, 3, 9, 6, 10],
+    ],
+    left: [[7]],
+  },
+  5: {
+    right: [
+      [3, 9],
+      [6, 10],
+    ],
+    left: [
+      [2, 8],
+      [4, 7],
+    ],
+  },
+  6: {
+    right: [[10]],
+    left: [
+      [3, 9],
+      [5, 2, 4, 8, 7],
+    ],
+  },
+  7: {
+    right: [[4], [2, 8, 5, 3, 9, 6, 10]],
+    left: [],
+  },
+  8: {
+    right: [[5], [3, 9, 6, 10]],
+    left: [[4], [7]],
+  },
+  9: {
+    right: [[6], [10]],
+    left: [[5], [2, 8, 4, 7]],
+  },
+  10: {
+    right: [],
+    left: [[6], [3, 9, 5, 2, 8, 4, 7]],
+  },
+};
 class Split {
-  private pinsCondition: number[];
-  private pinPosition: PinPosition = {
-    1: [7],
-    2: [4],
-    3: [2, 8],
-    4: [1, 5],
-    5: [3, 9],
-    6: [6],
-    7: [10],
-  };
-
-  constructor(pinsCondition: number[]) {
-    this.pinsCondition = pinsCondition;
+  private s: number[];
+  constructor(s: number[]) {
+    this.s = s;
   }
-
-  getPinPosition = () => {
-    return this.pinPosition;
-  };
-
-  splitJude = () => {
-    if (!this.pinsCondition[0]) {
+  jude() {
+    if (this.s[0]) {
       return "No";
     }
-  };
+    for (let pinIndex = 0; pinIndex < this.s.length; pinIndex++) {
+      if (pinIndex === 0) {
+        continue;
+      }
+      const pin_number = pinIndex + 1;
+      const { right, left } = pinPositionalRelationship[pin_number];
+      for (let i = 0; i < right.length; i++) {
+        const nextLanePins = right[0];
+        for (
+          let nextLanePinIndex = 0;
+          nextLanePinIndex < nextLanePins.length;
+          nextLanePinIndex++
+        ) {
+          const pinIndex = nextLanePins[nextLanePinIndex] - 1;
+          if (this.s[pinIndex]) {
+            return "No";
+          }
+        }
+        const otherThanRightBesideLanePins = right[1];
+        for (
+          let otherThanRightBesideLanePinIndex = 0;
+          otherThanRightBesideLanePinIndex <
+          otherThanRightBesideLanePins.length;
+          otherThanRightBesideLanePinIndex++
+        ) {
+          const pinIndex =
+            otherThanRightBesideLanePins[otherThanRightBesideLanePinIndex] - 1;
+          if (this.s[pinIndex]) {
+            return "Yes";
+          }
+        }
+      }
+    }
+  }
 }
 
 export default Split;
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,22 +1,25 @@
 import Split from "../src";
 
-test("ボーリングのピンと列の位置を紐付ける", () => {
-  const pinsCondition = [0, 1, 0, 1, 1, 1, 0, 1, 0, 1];
-  const split = new Split(pinsCondition);
-  const pinPosition = split.getPinPosition();
-  expect(pinPosition[1]).toEqual(expect.arrayContaining([7]));
-  expect(pinPosition[2]).toEqual(expect.arrayContaining([4]));
-  expect(pinPosition[3]).toEqual(expect.arrayContaining([2, 8]));
-  expect(pinPosition[4]).toEqual(expect.arrayContaining([1, 5]));
-  expect(pinPosition[5]).toEqual(expect.arrayContaining([3, 9]));
-  expect(pinPosition[6]).toEqual(expect.arrayContaining([6]));
-  expect(pinPosition[7]).toEqual(expect.arrayContaining([10]));
-});
-
 describe("split", () => {
-  test("1番目のピンが倒れていなかったらNO", () => {
-    const pinsCondition = [0, 1, 0, 1, 1, 1, 0, 1, 0, 1];
-    const split = new Split(pinsCondition);
-    expect(split.splitJude()).toBe("No");
+  test("pin1が倒れていなかったらNo", () => {
+    const s = [1, 1, 0, 1, 1, 1, 0, 1, 0, 1];
+    const split = new Split(s);
+    expect(split.jude()).toBe("No");
   });
+
+  test("1 番目のピンが倒れていたら次のピンで右方向でSplitじゃなかったらNo", () => {
+    const s = [0, 1, 1, 1, 1, 1, 0, 1, 0, 1];
+    const split = new Split(s);
+    expect(split.jude()).toBe("No");
+  });
+  test("1 番目のピンが倒れていたら次のピンで右方向でSplitだったらYes", () => {
+    const s = [0, 1, 1, 1, 0, 1, 0, 1, 0, 1];
+    const split = new Split(s);
+    expect(split.jude()).toBe("Yes");
+  });
+  // test("1 番目のピンが倒れていたら次のピンで左方向でSplitだったらYes", () => {
+  //   const s = [0, 1, 1, 0, 1, 1, 7, 1, 0, 1];
+  //   const split = new Split(s);
+  //   expect(split.jude()).toBe("Yes");
+  // });
 });
